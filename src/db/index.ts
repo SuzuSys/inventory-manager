ff5a06bf853ba907c517935ee5809711294f3887
SuzuSys <sigmatics@outlook.jp>
2025-03-04 14:15:03 +0900

    upate db schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,17 +2,18 @@ import Dexie, { type EntityTable } from "dexie";
 
 interface Inventory {
   id: number;
-  directory: string;
+  directory: string; // "/", "/冷蔵庫/"
   name: string;
   registrationDate: Date;
+  expirationDate: Date | null;
 }
 
 const db = new Dexie("Inventory") as Dexie & {
   inventory: EntityTable<Inventory, "id">;
 };
 
-db.version(1).stores({
-  inventory: "++id, &[directory+name], name, registrationDate",
+db.version(2).stores({
+  inventory: "++id, &[directory+name], name, registrationDate, expirationDate",
 });
 
 export type { Inventory };
--- src/pages/Database.vue
+++ src/pages/Database.vue
@@ -1,5 +1,25 @@
 <script lang="ts" setup>
 import { db } from "@/db";
+
+db.inventory.clear();
+
+async function getAll() {
+  console.log(await db.inventory.toArray());
+}
+
+getAll();
+
+async function addItem() {
+  const id = await db.inventory.add({
+    directory: "/",
+    name: "shldered",
+    registrationDate: new Date(),
+    expirationDate: null,
+  });
+  console.log(id);
+}
+
+addItem();
 </script>
 
 <template></template>
